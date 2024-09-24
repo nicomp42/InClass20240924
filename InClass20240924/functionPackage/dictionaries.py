3395c0449510a37e94e5e78a1c0c7abb7eef687d
willgpa <wgpadgett@gmail.com>
2024-09-24 08:57:53 -0400

    Made changes during class

--- InClass20240924/functionPackage/dictionaries.py
+++ InClass20240924/functionPackage/dictionaries.py
@@ -1,1 +1,44 @@
 # dictionaries.py
+
+
+def demo():
+    """
+    demonstrate basic disctionary stuff
+    """
+    myDictionary = {"Cincinnati":"Bearcats", "Xavier":"Musketeers", "NKU":"Norse", "UC Clermont":"Cougars"}
+    print(myDictionary)
+
+    #iterate over the dictionary by key
+    for key in myDictionary.keys():
+        print(key)
+
+    #iterate by key/value
+    for item in myDictionary.items():
+        print(item)
+    
+    for value in myDictionary.values():
+        print(value)
+
+    #add a key/value pair
+    myDictionary["Michigan State"] = "Spartans"
+    print(len(myDictionary))
+
+    #Cause an exception add try except logic to
+    #Gracefully handle this
+    try:
+        print(myDictionary["Ohio State"])
+    except:
+        print("Ohio State is an invalid key")
+
+    #remove xavier by key and print the entire dictionary
+
+    myDictionary.pop("Xavier")
+
+    print(myDictionary)
+
+    # Create another dictionary called newTeams
+    #Add several key value pairs
+    #combine that with myDictionary and print results
+    newTeams = {"Ohio State":"Buckeyes", "Indiana":"Hoosiers", "Purdue":"Boilermakers","Cincinnati":"Bearcats"}
+    myDictionary.update(newTeams)
+    print(myDictionary)
--- InClass20240924/mainPackage/main.py
+++ InClass20240924/mainPackage/main.py
@@ -1,2 +1,5 @@
 # main.py
-
+from functionPackage.dictionaries import *
+if __name__ == "__main__":
+    print("I am main.py")
+    demo()
